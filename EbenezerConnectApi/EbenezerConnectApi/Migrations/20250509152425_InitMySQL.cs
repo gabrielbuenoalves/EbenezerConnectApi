a641faaefa042900a610c913c04583908c9452ec
gabriel bueno <gabrielbuenoalves@outlook.com>
2025-05-09 14:39:03 -0300

    Migrations iniciais do novo banco

---
+++ EbenezerConnectApi/EbenezerConnectApi/Migrations/20250509152425_InitMySQL.cs
@@ -0,0 +1,251 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace EbenezerConnectApi.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitMySQL : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterDatabase()
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.CreateTable(
+                name: "ProdutoEstoque",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    Nome = table.Column<string>(type: "varchar(100)", maxLength: 100, nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    QuantidadeEmEstoque = table.Column<int>(type: "int", nullable: false),
+                    PrecoCompraAtual = table.Column<decimal>(type: "decimal(18,2)", nullable: false),
+                    PrecoVendaAtual = table.Column<decimal>(type: "decimal(18,2)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ProdutoEstoque", x => x.Id);
+                })
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.CreateTable(
+                name: "Quarto",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    Nome = table.Column<string>(type: "varchar(50)", maxLength: 50, nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    Capacidade = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Quarto", x => x.Id);
+                })
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.CreateTable(
+                name: "PrecoHistoricoProduto",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    ProdutoEstoqueId = table.Column<int>(type: "int", nullable: false),
+                    PrecoCompra = table.Column<decimal>(type: "decimal(18,2)", nullable: false),
+                    PrecoVenda = table.Column<decimal>(type: "decimal(18,2)", nullable: false),
+                    DataInicio = table.Column<DateTime>(type: "datetime(6)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_PrecoHistoricoProduto", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_PrecoHistoricoProduto_ProdutoEstoque_ProdutoEstoqueId",
+                        column: x => x.ProdutoEstoqueId,
+                        principalTable: "ProdutoEstoque",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                })
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.CreateTable(
+                name: "Pessoa",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    Funcao = table.Column<string>(type: "varchar(50)", maxLength: 50, nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    Nome = table.Column<string>(type: "varchar(100)", maxLength: 100, nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    Cpf = table.Column<string>(type: "varchar(11)", maxLength: 11, nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    Email = table.Column<string>(type: "varchar(100)", maxLength: 100, nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    SenhaHash = table.Column<string>(type: "longtext", nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    Igreja = table.Column<string>(type: "varchar(100)", maxLength: 100, nullable: true)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    Saldo = table.Column<decimal>(type: "decimal(18,2)", nullable: false),
+                    QuartoId = table.Column<int>(type: "int", nullable: true),
+                    SenhaTemporariaHash = table.Column<string>(type: "longtext", nullable: true)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    EmailConfirmado = table.Column<bool>(type: "tinyint(1)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Pessoa", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Pessoa_Quarto_QuartoId",
+                        column: x => x.QuartoId,
+                        principalTable: "Quarto",
+                        principalColumn: "Id");
+                })
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.CreateTable(
+                name: "EmailConfirmacao",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    Token = table.Column<string>(type: "longtext", nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    ExpiraEm = table.Column<DateTime>(type: "datetime(6)", nullable: false),
+                    PessoaId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_EmailConfirmacao", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_EmailConfirmacao_Pessoa_PessoaId",
+                        column: x => x.PessoaId,
+                        principalTable: "Pessoa",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                })
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.CreateTable(
+                name: "TransacaoCantina",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    PessoaId = table.Column<int>(type: "int", nullable: false),
+                    DataTransacao = table.Column<DateTime>(type: "datetime(6)", nullable: false),
+                    Valor = table.Column<decimal>(type: "decimal(18,2)", nullable: false),
+                    Descricao = table.Column<string>(type: "varchar(255)", maxLength: 255, nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    Tipo = table.Column<string>(type: "varchar(50)", maxLength: 50, nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4")
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_TransacaoCantina", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_TransacaoCantina_Pessoa_PessoaId",
+                        column: x => x.PessoaId,
+                        principalTable: "Pessoa",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                })
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.CreateTable(
+                name: "ItemTransacaoCantina",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    TransacaoCantinaId = table.Column<int>(type: "int", nullable: false),
+                    ProdutoEstoqueId = table.Column<int>(type: "int", nullable: false),
+                    Quantidade = table.Column<int>(type: "int", nullable: false),
+                    PrecoVendaUnitario = table.Column<decimal>(type: "decimal(18,2)", nullable: false),
+                    PrecoCompraUnitario = table.Column<decimal>(type: "decimal(18,2)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ItemTransacaoCantina", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_ItemTransacaoCantina_ProdutoEstoque_ProdutoEstoqueId",
+                        column: x => x.ProdutoEstoqueId,
+                        principalTable: "ProdutoEstoque",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_ItemTransacaoCantina_TransacaoCantina_TransacaoCantinaId",
+                        column: x => x.TransacaoCantinaId,
+                        principalTable: "TransacaoCantina",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                })
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_EmailConfirmacao_PessoaId",
+                table: "EmailConfirmacao",
+                column: "PessoaId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ItemTransacaoCantina_ProdutoEstoqueId",
+                table: "ItemTransacaoCantina",
+                column: "ProdutoEstoqueId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ItemTransacaoCantina_TransacaoCantinaId",
+                table: "ItemTransacaoCantina",
+                column: "TransacaoCantinaId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Pessoa_Cpf",
+                table: "Pessoa",
+                column: "Cpf",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Pessoa_QuartoId",
+                table: "Pessoa",
+                column: "QuartoId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_PrecoHistoricoProduto_ProdutoEstoqueId",
+                table: "PrecoHistoricoProduto",
+                column: "ProdutoEstoqueId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_TransacaoCantina_PessoaId",
+                table: "TransacaoCantina",
+                column: "PessoaId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "EmailConfirmacao");
+
+            migrationBuilder.DropTable(
+                name: "ItemTransacaoCantina");
+
+            migrationBuilder.DropTable(
+                name: "PrecoHistoricoProduto");
+
+            migrationBuilder.DropTable(
+                name: "TransacaoCantina");
+
+            migrationBuilder.DropTable(
+                name: "ProdutoEstoque");
+
+            migrationBuilder.DropTable(
+                name: "Pessoa");
+
+            migrationBuilder.DropTable(
+                name: "Quarto");
+        }
+    }
+}
